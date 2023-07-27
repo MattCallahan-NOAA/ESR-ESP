0664aa4d573e5f16da04349094108676b4ade8d3
MattCallahan-NOAA <matt.callahan@noaa.gov>
2023-07-26 16:33:40 -0800

    Downloaded all data and grouped by years.
    This is only borderline reproducible since order numbers will change.

--- Chla/globcolour_download.R
+++ Chla/globcolour_download.R
@@ -103,6 +103,7 @@ filenames <- getURL(url, userpwd = userpwd,
                     ftp.use.epsv = FALSE,dirlistonly = TRUE)
 # unlisting all files
 filenames<-unlist(strsplit(filenames, "\r\n"))
+#only 1105... will QA to make sure I'm not missing dates.
 
 ## downloading all the nc files from the ftp server - saving names
 for(i in 1:length(filenames)) {
@@ -112,14 +113,77 @@ for(i in 1:length(filenames)) {
 }
 
 
-saveRDS(filenames,"data/2023_w/filenames_2023_w.RDS")
+saveRDS(filenames,"data/2023_w/filenames_2023_w_900805022.RDS")
 
 
-###
-### unpacking nc and converting to rds objects
-###
+#second order
+#deleted "list.txt" so it doesn't fail
+order_number<-787293221
+url<-paste0("ftp://ftp_hermes@ftp.hermes.acri.fr/",order_number,"/") # my full ftp link
 
-filenames_extract<-readRDS("data/2023_w/filenames_2023_w.RDS")
+#userpwd<- "ftp_gc_MCallahan:MCallahan_5113"
+userpwd <- "ftp_hermes:hermes%"
+filenames <- getURL(url, userpwd = userpwd,
+                    ftp.use.epsv = FALSE,dirlistonly = TRUE)
+# unlisting all files
+filenames<-unlist(strsplit(filenames, "\r\n"))
+
+## downloading all the nc files from the ftp server - saving names
+for(i in 1:length(filenames)) {
+  GET((paste0(url, filenames[i])),
+      authenticate("ftp_hermes","hermes%"),
+      write_disk(path=paste0("data/2023_w/787293221/",filenames[i])))
+}
+
+
+saveRDS(filenames,"data/2023_w/filenames_2023_w_787293221.RDS")
+
+
+### check file list
+wai1<-readRDS("data/2023_w/filenames_2023_w_900805022.RDS")
+wai1<-wai1[!grepl(".txt", wai1)]
+wai1<-wai1[!grepl("AV", wai1)]
+
+wai2<-readRDS("data/2023_w/filenames_2023_w_787293221.RDS") 
+  wai2<-wai2[!grepl(".txt", wai2)]
+wai2<-wai2[!grepl("AV", wai2)]
+
+wai_all<-c(wai1, wai2)
+#just save dates
+wai_all<-substr(wai_all, 5,12)
+wai_all<-as.Date(wai_all, format="%Y%m%d")
+
+#no duplicate dates
+any(duplicated(wai_all))
+
+#convert to dates
+wai_all=as.Date(wai_all, format= )
+#check sequence of dates
+min(wai_all); max(wai_all)
+#There are 100 more days in an ideal sequence of dates...
+# not too surprising that the entire aleutians would be obscured for weeks at a time though.
+length(seq.Date(as.Date("1997/09/06"), as.Date("2023/07/12"), by=8))
+
+wai_all<-as.data.frame(wai_all)
+wai_all <-wai_all %>%
+  mutate(year=year(wai_all),
+         month=month(wai_all))
+
+#cool all years have 11 months in April-June. 
+wai_all %>%
+  filter(month %in% c(4,5,6)) %>%
+  group_by(year) %>%
+  summarize(n()) %>%
+  print(n=Inf)
+
+#missing weeks
+wai_missing<-wai_all[!(wai_all %in% ideal)]
+
+#now proceeding with extraction
+
+#first batch
+
+filenames_extract<-readRDS("data/2023_w/filenames_2023_w_900805022.RDS")
 filenames_extract<-filenames_extract[!grepl(".txt", filenames_extract)] # don't extract the text file 
 filenames_extract_av <- filenames_extract[grepl("AV", filenames_extract)] # only need to save the av files as RDS
 
@@ -134,15 +198,35 @@ for(y  in 1:(length(filenames_extract_av))) {
   rm(d)
 }
 
-#pulling this out of the loop so I can get rid of the non AV files too
-#file.remove(paste0("data/2023_w/",filenames_extract)) 
+
+#second batch
+filenames_extract<-readRDS("data/2023_w/filenames_2023_w_787293221.RDS")
+filenames_extract<-filenames_extract[!grepl(".txt", filenames_extract)] # don't extract the text file 
+filenames_extract_av <- filenames_extract[grepl("AV", filenames_extract)] # only need to save the av files as RDS
+
+for(y  in 1:(length(filenames_extract_av))) {
+  input_nc_fileName<-filenames_extract_av[y]
+  d<-tidync(paste0("data/2023_w/787293221/",input_nc_fileName)) %>%
+    hyper_tibble(na.rm=FALSE)
+  d$id<-paste(input_nc_fileName) # you can then get the date
+  rds_file_name<-paste0("data/2023_w/",str_sub(input_nc_fileName, end=-4), ".RDS")
+  saveRDS(d, file = rds_file_name)    # assign(paste("df", y, sep=""), df)
+  rm(d)
+}
 
 # select filenames
 
 all_RDS<-list.files(path=here("data", "2023_w"), pattern = ".RDS")
+#remove those filename lists
+all_RDS<-all_RDS[!grepl("filenames", all_RDS)]
 
 # Combine into annual files 
 
 rds_fx<-function(year){
-  saveRDS(purrr:: map_dfr(all_RDS[substr(all_RDS, 5, 8)==year], readRDS), file = paste0("data/2023_w/",year,'_glob_w.RDS')) #
+  saveRDS(purrr:: map_dfr(paste0("data/2023_w/",all_RDS[substr(all_RDS, 5, 8)==year]), readRDS), file = paste0("data/2023_w/",year,'_glob_w.RDS')) #
 }
+
+rds_fx(1997)
+
+#do for all years
+lapply(1998:2023, function(x) rds_fx(x))
