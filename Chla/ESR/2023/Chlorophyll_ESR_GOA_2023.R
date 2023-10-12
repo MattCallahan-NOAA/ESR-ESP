1c43df7bdd3847194cca43ce7450af1d5af0c7b8
MattCallahan-NOAA <matt.callahan@noaa.gov>
2023-10-12 15:40:58 -0800

    GOA chla figure edits

--- Chla/ESR/2023/Chlorophyll_ESR_GOA_2023.R
+++ Chla/ESR/2023/Chlorophyll_ESR_GOA_2023.R
@@ -114,8 +114,8 @@ dev.off()
 
 #load 508 complient colors
 current.year.color <- "black"
-last.year.color <- '#4C9C2E'
-mean.color <- '#1ECAD3'
+last.year.color <- '#0093D0'
+mean.color <- '#7F7FFF'
 old.years.color<-'#D0D0D0'
 
 
@@ -164,6 +164,61 @@ ggplot() +
         axis.text = element_text(size=10,family="sans"),
         #panel.border=element_rect(colour="black",size=0.75),
        # axis.text.x=element_text(color=c("black",NA,NA,"black",NA,NA,"black",NA,NA,"black",NA,NA,NA)),
+        legend.key.size = unit(0.35,"cm"),
+        plot.margin=unit(c(0.65,0,0.65,0),"cm")) 
+dev.off()
+
+
+#plot smoothed version
+png("ESR/2023/Chla_annual_lines_smoothed.png",width=7,height=5,units="in",res=300)
+ggplot() +
+  geom_smooth(data=data %>% filter(year<last.year),
+            aes(doy,meanchla,group=factor(year),col='old.years.color'),size=0.3, method="loess", se=FALSE) +
+  geom_smooth(data=data %>% filter(year==last.year),
+            aes(doy,meanchla,color='last.year.color'),size=0.75, method="loess", se=FALSE) +
+  geom_smooth(data=data %>% 
+              filter(year%in%mean.years) %>% 
+              group_by(ecosystem_subarea, doy) %>% 
+              summarise(meanchla=mean(meanchla,na.rm=TRUE)),
+            aes(doy,meanchla,col='mean.color'),size=0.75, method="loess", se=FALSE) +
+  geom_smooth(data=data %>% filter(year==current.year),
+            aes(doy,meanchla,color='current.year.color'),size=0.95, method="loess", se=FALSE) +
+  facet_wrap(~ecosystem_subarea ,ncol=2) + 
+  scale_color_manual(name="",
+                     breaks=c('current.year.color','last.year.color','old.years.color','mean.color'),
+                     values=c('current.year.color'=current.year.color,'last.year.color'=last.year.color,'old.years.color'=old.years.color,'mean.color'=mean.color),
+                     labels=c(current.year,last.year,paste0('1998-',last.year-1),mean.lab)) +
+  ylab("mean Chla (ug/L)") + 
+  xlab("") +
+  scale_x_continuous(breaks=c(91,121,152), labels=c("Apr", "May", "Jun"))+
+  theme(legend.position=c(mylegx,mylegy),
+        legend.text = element_text(size=8,family="sans"),
+        legend.background = element_blank(),
+        legend.title = element_blank(),
+        strip.text = element_text(size=10,color="white",family="sans",face="bold"),
+        strip.background = element_rect(fill=OceansBlue2),
+        axis.title = element_text(size=10,family="sans"),
+        axis.text = element_text(size=10,family="sans"),
+        legend.key.size = unit(0.35,"cm"),
+        plot.margin=unit(c(0.65,0,0.65,0),"cm")) 
+dev.off()
+
+# plot all years
+png("ESR/2023/Chla_yearbyyear_lines.png",width=7,height=20,units="in",res=300)
+ggplot() +
+  geom_line(data=data,
+            aes(doy,meanchla),color="red",size=0.95) +
+  facet_grid(rows=vars(year), cols=vars(ecosystem_subarea)) + 
+  ylab("mean Chla (ug/L)") + 
+  xlab("") +
+  scale_x_continuous(breaks=c(91,121,152), labels=c("Apr", "May", "Jun"))+
+  theme(
+        strip.text = element_text(size=12,color="white",family="sans",face="bold"),
+        strip.background = element_rect(fill=OceansBlue2),
+        axis.title = element_text(size=12,family="sans"),
+        axis.text = element_text(size=10,family="sans"),
+        panel.border=element_rect(colour="black",size=0.25),
+        # axis.text.x=element_text(color=c("black",NA,NA,"black",NA,NA,"black",NA,NA,"black",NA,NA,NA)),
         legend.key.size = unit(0.35,"cm"),
         plot.margin=unit(c(0.65,0,0.65,0),"cm")) 
 dev.off()
