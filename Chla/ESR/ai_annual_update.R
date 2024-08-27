fe94b9229ba63f68591221e5ce914b3af8e419e5
MattCallahan-NOAA <matt.callahan@noaa.gov>
2024-08-27 09:29:36 -0800

    AI data pull

--- Chla/ESR/ai_annual_update.R
+++ Chla/ESR/ai_annual_update.R
@@ -1,15 +1,25 @@
 library(tidyverse)
 library(lubridate)
-library(odbc)
+library(RJDBC)
 library(getPass)
 
-#connect to akfin
-con <- dbConnect(odbc::odbc(), "akfin", UID=getPass(msg="USER NAME"), PWD=getPass()) 
+options(java.parameters="-Xmx12g")
+
+# connect to AKFIN
+jdbcDriver <- JDBC(driverClass="oracle.jdbc.OracleDriver", classPath="../../snippets/dbconnect/java/ojdbc8.jar")
+
+con <- dbConnect(jdbcDriver, 
+                   "jdbc:oracle:thin:@//tiger:2045/akfin.psmfc.org", 
+                   getPass(msg="USER NAME"), getPass())
+
+
+current_year <- 2024
+last_year <- 2023
+
 
 #spring
-# Note, reran in 2024
 start<-Sys.time()
-data<-dbFetch(dbSendQuery(con, "select round(chla,2) chla, 
+spring<-dbFetch(dbSendQuery(con, paste0("select round(chla,2) chla, 
                           to_date(start_date,'YYYY-MM-DD')+4 mid_date, 
                           chla_lat,
                           chla_lon,
@@ -20,31 +30,18 @@ data<-dbFetch(dbSendQuery(con, "select round(chla,2) chla,
 from env_data.globcolour a
 left join env_data.globcolour_spatial_lookup b on a.glob_id=b.glob_id
 where extract(month from to_date(start_date,'YYYY-MM-DD')+4) in (4, 5, 6)
-and ecosystem_area = ('Aleutian Islands')"))%>%
+and extract(year from to_date(start_date,'YYYY-MM-DD')+4) = ", current_year, "
+and ecosystem_area = ('Aleutian Islands')")))%>%
   rename_with(tolower)
 end<-Sys.time()
 end-start
-#15 minute download
+#1.5 minute for one year
 
-#This csv is too big to open or upload to google drive
-#write.csv(data, "data/ai_chla_2023.csv", row.names=FALSE)
-
-
-data<-data%>%
-  mutate(year=year(mid_date))
-
-myyears<-1998:2023
-
-for (i in myyears) {
-  data%>%
-    filter(year==i)%>%
-    write.csv(paste0("data/Aleutians/ai_glob_spring_",i,".csv"), row.names=FALSE)
-}
 
 #fall 
 #took 30 min!
 start<-Sys.time()
-data<-dbFetch(dbSendQuery(con, "select round(chla,2) chla, 
+fall<-dbFetch(dbSendQuery(con, paste0("select round(chla,2) chla, 
                           to_date(start_date,'YYYY-MM-DD')+4 mid_date, 
                           chla_lat,
                           chla_lon,
@@ -52,27 +49,23 @@ data<-dbFetch(dbSendQuery(con, "select round(chla,2) chla,
                           depth,
                           waters_cod state_fed,
                           stat_area
-from env_data.globcolour_2023 a
+from env_data.globcolour a
 left join env_data.globcolour_spatial_lookup b on a.glob_id=b.glob_id
 where extract(month from to_date(start_date,'YYYY-MM-DD')+4) in (8, 9, 10)
-and ecosystem_area = ('Aleutian Islands')"))%>%
+and extract(year from to_date(start_date,'YYYY-MM-DD')+4) = ", last_year, "
+and ecosystem_area = ('Aleutian Islands')")))%>%
   rename_with(tolower)
 end<-Sys.time()
 end-start
 
 
-#This csv is too big to open or upload to google drive
-#write.csv(data, "data/ai_chla_2023.csv", row.names=FALSE)
-
-
-data<-data%>%
+fall <- fall %>%
   mutate(year=year(mid_date))
 
-myyears<-1998:2023
+spring <- spring %>%
+  mutate(year=year(mid_date))
 
-for (i in myyears) {
-  data%>%
-    filter(year==i)%>%
-    write.csv(paste0("data/ai2023/ai_glob_fall_",i,".csv"), row.names=FALSE)
-}
+
+    write.csv(fall, paste0("data/Aleutians/ai_glob_fall_",last_year,".csv"), row.names=FALSE)
+    write.csv(spring, paste0("data/Aleutians/ai_glob_spring_",current_year,".csv"), row.names=FALSE)
 
