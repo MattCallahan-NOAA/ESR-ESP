fff8064fe20c96fa44e2203c7afc8ba4100bce04
MattCallahan-NOAA <matt.callahan@noaa.gov>
2025-09-12 11:55:11 -0800

    Added tanner indicator

--- Chla/ESP/2025_indicators/crab_occci.R
+++ Chla/ESP/2025_indicators/crab_occci.R
@@ -9,9 +9,10 @@ library(akfinupload)
 
 jdbcDriver <- RJDBC::JDBC(driverClass="oracle.jdbc.OracleDriver",
                           classPath=system.file("driver", "ojdbc8.jar", package = "akfinupload") )
-#server<-"jdbc:oracle:thin:@akfin"
+server<-"jdbc:oracle:thin:@akfin" #NOAA
+#server <- "jdbc:oracle:thin:@//tiger:2045/akfin.psmfc.org" #PSMFC
 
-con<-dbConnect(jdbcDriver, "jdbc:oracle:thin:@//tiger:2045/akfin.psmfc.org", key_list("akfin_oracle_db")$username, keyring::key_get("akfin_oracle_db", keyring::key_list("akfin_oracle_db")$username))
+con<-dbConnect(jdbcDriver, server, key_list("akfin_oracle_db")$username, keyring::key_get("akfin_oracle_db", keyring::key_list("akfin_oracle_db")$username))
 
 
 # AMJ_Chlorophylla_Biomass_SEBS_Satellite
@@ -41,3 +42,16 @@ dbGetQuery(con, bbrkc) %>%
   write.csv("ESP/2025_indicators/Spring_Chlorophylla_Biomass_SEBS_Inner_Shelf_Satellite.csv", row.names=FALSE)
 
 
+# Tanner
+# AMJJ_Chlorophylla_Concentration_Tanner_OCCCI
+# dropping July since it's missing in 2025
+tanner<-"select extract(year from to_date(read_date,'YYYY-MM-DD')+4) year, 'AMJJ_Chlorophylla_Concentration_Tanner_OCCCI' as indicator_name, round(avg(chlorophyll),2) data_value
+from env_data.OCCCI_CHLA a
+left join env_data.OCCCI_SPATIAL_LOOKUP b on a.occci_id=b.occci_id
+where extract(month from to_date(read_date,'YYYY-MM-DD')+4) in (4, 5, 6)
+and bsierp_id in (3,4,5,6,8)
+group by extract(year from to_date(read_date,'YYYY-MM-DD')+4)
+order by extract(year from to_date(read_date,'YYYY-MM-DD')+4) asc"
+
+dbGetQuery(con, tanner) %>%
+  write.csv("ESP/2025_indicators/AMJJ_Chlorophylla_Concentration_Tanner_OCCCI.csv", row.names=FALSE)
